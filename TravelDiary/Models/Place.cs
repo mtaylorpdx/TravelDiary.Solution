b5accefff824dd5b2c7ba053213b69bf03aa5144
mtaylorpdx <taylor.matt@protonmail.com>
2020-03-11 09:01:29 -0700

    expand Place class

--- TravelDiary/Models/Place.cs
+++ TravelDiary/Models/Place.cs
@@ -5,5 +5,16 @@ namespace TravelDiary.Models
   public class Place
   {
     public string CityName { get; set; }
+    private static List<Place> _instances = new List<Place> {};
+
+    public Place()
+    {
+      _instances.Add(this);
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
   }
 }
